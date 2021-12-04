f777191c8d62d119831bd5275abca1de8a415130
tuyen-at-work <68000455+tuyen-at-work@users.noreply.github.com>
2021-12-04 08:09:29 +0700

    [*] Update HtmlHelperExtensions

--- src/AtomicDesignDemo/Extensions/HtmlHelperExtensions.cs
+++ src/AtomicDesignDemo/Extensions/HtmlHelperExtensions.cs
@@ -4,32 +4,40 @@ using System.Web.Mvc;
 using EPiServer.ServiceLocation;
 using EPiServer.Web.Hosting;
 using HandlebarsDotNet;
+// ReSharper disable InvertIf
 
 namespace AtomicDesignDemo.Extensions
 {
     public static class HtmlHelperExtensions
     {
-        private static readonly Dictionary<string, HandlebarsTemplate<object, object>> Templates = new Dictionary<string, HandlebarsTemplate<object, object>>();
+        private static readonly Dictionary<string, HandlebarsTemplate<object, object>> _templates = new Dictionary<string, HandlebarsTemplate<object, object>>();
 
-        public static MvcHtmlString RenderTemplate(this HtmlHelper helper, string templateName, object context)
+        public static MvcHtmlString RenderTemplate(
+            this HtmlHelper helper,
+            string templateName,
+            object context)
         {
-            if (!Templates.ContainsKey(templateName))
+            if (!_templates.ContainsKey(templateName))
             {
-                var hostingEnvironment = ServiceLocator.Current.GetInstance<IHostingEnvironment>();
-                var patternFile = hostingEnvironment.MapPath($"~/assets/patterns/{templateName}.hbs");
+                var hostingEnvironment = ServiceLocator.Current
+                    .GetInstance<IHostingEnvironment>();
+                var patternFile = hostingEnvironment
+                    .MapPath($"~/assets/patterns/{templateName}.hbs");
                 if (File.Exists(patternFile))
                 {
                     var pattern = File.ReadAllText(patternFile);
                     var template = Handlebars.Compile(pattern);
-                    Templates[templateName] = template;
+                    _templates[templateName] = template;
                 }
             }
-            if (Templates.ContainsKey(templateName))
+
+            if (_templates.ContainsKey(templateName))
             {
-                var template = Templates[templateName];
+                var template = _templates[templateName];
                 var result = template(context);
                 return MvcHtmlString.Create(result);
             }
+
             return MvcHtmlString.Empty;
         }
     }
